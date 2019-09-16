b20077e8009d1a7a0591803fccf68996c57773f8
Rajithakumar <rkumar@agero.com>
2019-09-16 12:48:04 +0530

    new update

--- src/newpackage/MyClass.java
+++ src/newpackage/MyClass.java
@@ -1,26 +1,26 @@
 package newpackage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.testng.annotations.*;
 public class MyClass {
-	static WebDriver driver;
 	
-	@BeforeSuite
-	public static void setup(){
-		 driver = new ChromeDriver();
-	System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
-	driver.get("http://aflstems.accelfrontline.com");
+	public static void main (String[] args){
+		
+//		
+	System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.25.0-win32\\geckodriver.exe");
+	
+	WebDriver driver = new FirefoxDriver();
+	
+	
+	driver.get("http://google.com");
 	driver.manage().window().maximize();
 	
 	
 	}
-	
-	@Test
-	
-	public void launch(){
-		
-		
-	}
+
 
 }
