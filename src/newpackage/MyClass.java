f2e69c586a2d79c4978cf0fed6929e680ba65e0b
Rajithakumar <rkumar@agero.com>
2019-09-16 16:39:04 +0530

    new image

--- src/newpackage/MyClass.java
+++ src/newpackage/MyClass.java
@@ -1,23 +1,23 @@
 package newpackage;
 
-import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.WebDriver;
+////import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
 //import org.testng.annotations.*;
 public class MyClass {
 	
-	public static void main (String[] args){
-		
+	public static void main(String[] args){
+		System.out.println("Helloworld");
 //		
-	System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.25.0-win32\\geckodriver.exe");
-	
-	WebDriver driver = new FirefoxDriver();
-	
-	
-	driver.get("http://google.com");
-	driver.manage().window().maximize();
+//	System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.25.0-win32\\geckodriver.exe");
+//	
+//	WebDriver driver = new FirefoxDriver();
+//	
+//	
+//	driver.get("http://google.com");
+//	driver.manage().window().maximize();
 	
 	
 	}
